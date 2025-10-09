a53f094d95ce6934080a7df6e554fdbe74f36f31
Kashyap Sukshavasi <ksukshavasi@gmail.com>
2025-10-08 23:57:04 -0400

    [fix] Updated preloader to have proper data

--- preload.js
+++ preload.js
@@ -4,7 +4,7 @@ contextBridge.exposeInMainWorld('nestApi', {
     loadConfig: () => ipcRenderer.invoke('cfg-load'),
     saveConfig: (data) => ipcRenderer.invoke('cfg-save', data),
     startSession: (user) => ipcRenderer.invoke('conn-start', user),
-    sendInput: (text) => ipcRenderer.send('pty-input', txt),
-    onData: (txt) => ipcRenderer.on('pty-data', (e, d) => cb(d)),
+    sendInput: (txt) => ipcRenderer.send('pty-input', txt),
+    onData: (cb) => ipcRenderer.on('pty-data', (e, d) => cb(d)),
     resize: (cols, rows) => ipcRenderer.invoke('pty-resize', { cols, rows })
 })
